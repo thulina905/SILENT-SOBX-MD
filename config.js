6a275063833459d3b7a367d4f53782c898084488
thulina905 <santathulina0@gmail.com>
2024-12-24 00:33:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "irxwRYjB#m1IlfqFIIjFjXmsdUwju5zT1ULczR5UU0NVRkf63H6I",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
